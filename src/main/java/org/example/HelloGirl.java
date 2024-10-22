278478806b4e91ac1bc5044524e818b02ad03117
Shefira Marizcha <shefiramarizcha62@gmail.com>
2024-10-22 12:09:42 +0700

    Added a simple Java program

---
+++ src/main/java/org/example/HelloGirl.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class HelloGirl {
+    // Metode untuk menampilkan pesan
+    public void sayHello() {
+        System.out.println("Hello Girl");
+    }
+
+    public static void main(String[] args) {
+        // Membuat objek dari class HelloGirl
+        HelloGirl hello = new HelloGirl();
+
+        // Memanggil metode untuk menampilkan pesan
+        hello.sayHello();
+    }
+}
